f9e192cda6fdba923ee47e34aef2442facd5f062
Christina Dob <cd@cd.local>
2019-04-03 12:55:47 -0700

    Week 5 Day 3 Mini

---
+++ sql mini.sql
@@ -0,0 +1,17 @@
+select * from artist;
+
+SELECT first_name, last_name, country
+FROM employee;
+
+SELECT name, composer, milliseconds
+FROM track
+WHERE milliseconds > 299000;
+
+
+SELECT count(*)
+FROM track
+WHERE milliseconds > 299000;
+
+
+
+
